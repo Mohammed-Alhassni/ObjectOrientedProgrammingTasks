b27bee346a497c87105aa9fece87c7c07255f2a7
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-06-16 10:51:33 +0400

    Implemeted the display option, and renamed loop flag to interacting.

--- OOP_Part1/Program.cs
+++ OOP_Part1/Program.cs
@@ -8,20 +8,64 @@
             List<Guest> guest= new List<Guest>();
 
             int option = -1;
-            bool isMenuOn = true;
+            bool interacting = true;
 
-            while (isMenuOn)
+            while (interacting)
             {
+                Console.Clear();
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("""
+                                        ================================================
+                                        GRAND VISTA HOTEL — MANAGEMENT SYSTEM
+                                        ================================================
+                    """);
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("""
+                                                1. Add New Room
+                                                2. Register New Guest
+                                                3. Book a Room for a Guest
+                    """);
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("""
+                                                4. Search & Filter Rooms
+                                                5. Guest & Booking Statistics
+                    """);
+
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("""
+                                                6. Check Out a Guest
+                                                7. Remove Unavailable Rooms
+                    """);
+
+                Console.ResetColor();
+                Console.WriteLine("""
+                                                0. Exit
+                    """);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("""
+                                        ================================================
+                    """);
+
+                Console.ResetColor();
+
+                Console.Write("Enter your choice: ");
+                option = int.Parse(Console.ReadLine());
+
                 switch (option)
                 {
                     case 1:
 
                         break;
                     case 0:
-
+                        interacting = false;
                         break;
                     default:
                         Console.WriteLine("Invalid Option");
+                        Thread.Sleep(2000);
                         break;
                 }          
             }
