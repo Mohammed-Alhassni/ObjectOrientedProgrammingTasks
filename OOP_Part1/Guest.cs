92edf24f21e9fa3e8d2f92e7aec546de69f68722
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-06-16 17:21:12 +0400

    implemented a parametterized constructor in guest class and changed guest id to string.

--- OOP_Part1/Guest.cs
+++ OOP_Part1/Guest.cs
@@ -6,14 +6,20 @@ namespace OOP_Part1
 {
     internal class Guest
     {
-        public int GuestId { get; set; }
+        public string GuestId { get; set; }
         public string GuestName { get; set; }
         public int RoomNumber { get; set; }
         public DateTime CheckinDate { get; set; }
         public int TotalNights { get; set; }
 
-        public void DisplayGuest()
+        public Guest(string id, string name, int nights, int roomNumber = 0, DateTime date = new DateTime())
         {
+            GuestId = id;
+            GuestName = name;
+            RoomNumber = roomNumber;
+            CheckinDate = date;
+            TotalNights = nights;
+        }
 
         }
 
