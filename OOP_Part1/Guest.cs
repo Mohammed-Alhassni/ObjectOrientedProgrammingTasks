7f6c21b17c7f53570a79f8f96e406795e4d8339a
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-06-16 09:43:11 +0400

    Added Guest and Room classes

---
+++ OOP_Part1/Guest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Part1
+{
+    internal class Guest
+    {
+    }
+}
---
+++ OOP_Part1/Room.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Part1
+{
+    internal class Room
+    {
+
+    }
+}
