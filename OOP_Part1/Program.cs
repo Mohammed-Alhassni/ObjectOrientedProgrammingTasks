2fe4f1a481c5e6f603f690a9ef0f0d4a2d0369cc
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-06-16 10:02:48 +0400

    Added template while loop and switch for the main menu.

--- OOP_Part1/Program.cs
+++ OOP_Part1/Program.cs
@@ -7,6 +7,25 @@
             List<Room> rooms= new List<Room>();
             List<Guest> guest= new List<Guest>();
 
+            int option = -1;
+            bool isMenuOn = true;
+
+            while (isMenuOn)
+            {
+                switch (option)
+                {
+                    case 1:
+
+                        break;
+                    case 0:
+
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }          
+            }
+
         }
     }
 }
