4c0aeff6656bdf8fe8918fb138e07b06dc4bf8a6
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-06-16 09:54:01 +0400

    Added initial properties and methods for Guest

--- OOP_Part1/Guest.cs
+++ OOP_Part1/Guest.cs
@@ -6,5 +6,20 @@ namespace OOP_Part1
 {
     internal class Guest
     {
+        public int GuestId { get; set; }
+        public string GuestName { get; set; }
+        public int RoomNumber { get; set; }
+        public DateTime CheckinDate { get; set; }
+        public int TotalNights { get; set; }
+
+        public void DisplayGuest()
+        {
+
+        }
+
+        public double CalculateTotalCost()
+        {
+            return 0;
+        }
     }
 }
