9141aaaf4b31467fddcdfd22f0b34fd5a90352b3
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-06-16 15:05:29 +0400

    Implemented a parameterized constructor for Room class.

--- OOP_Part1/Room.cs
+++ OOP_Part1/Room.cs
@@ -9,6 +9,14 @@ namespace OOP_Part1
         public int RoomNumber { get; set; }
         public string RoomType { get; set; }
         public double PricePerNight { get; set; }
+
+        public Room(int number, string type, double price)
+        {
+            RoomNumber = number;
+            RoomType = type;
+            PricePerNight = price;
+        }
+
         public string DisplayRoom(bool show= true)
         {
             string newRoom= $"""
